aff05c43ccbac3a06a1d12c35809ae6f1028c7c8
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-02-20 11:23:46 +0630

    add link to register page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "primereact/button";
 import { LoginForm as components } from "@/constants/Auth";
 import { login } from "@/services/authServices";
 import { FormFieldType } from "@/types/form";
+import Link from "next/link";
 
 const Login = () => {
   const methods = useForm();
@@ -17,21 +18,35 @@ const Login = () => {
   });
 
   return (
-    <FormProvider {...methods}>
-      <form
-        action={action}
-        className="h-[100vh] flex flex-col gap-4 justify-center items-center m-auto">
-        {components?.map((component, index) => (
-          <DynamicFormElement
-            key={index}
-            componentType={component.formProps.type as FormFieldType}
-            {...component}
-            control={control}
+    <div className="h-[100vh] flex flex-col gap-4 justify-center items-center m-auto">
+      <FormProvider {...methods}>
+        <form
+          action={(e) => {
+            console.log("xxx form event ", e);
+            action();
+          }}>
+          <div className="grid gap-4">
+            {components?.map((component, index) => (
+              <DynamicFormElement
+                key={index}
+                componentType={component.formProps.type as FormFieldType}
+                {...component}
+                control={control}
+              />
+            ))}
+          </div>
+          <Button
+            type="submit"
+            label="Login"
+            className="w-full text-center my-4"
           />
-        ))}
-        <Button type="submit" label="Login" />
-      </form>
-    </FormProvider>
+        </form>
+      </FormProvider>
+      <div> {`Don't have an account?`} </div>
+      <Link href="/auth/register" className="text-blue-500">
+        Register here
+      </Link>
+    </div>
   );
 };
 
