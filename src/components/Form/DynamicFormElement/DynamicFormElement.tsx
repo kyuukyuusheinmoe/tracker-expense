3ddd30ab504fb940086c066980b9a7629266877a
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-03-26 20:12:52 +0630

    update client component

--- src/components/Form/DynamicFormElement/DynamicFormElement.tsx
+++ src/components/Form/DynamicFormElement/DynamicFormElement.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { useController, useFormContext } from "react-hook-form";
 import { componentMapper } from "@/utils/componentMapper";
