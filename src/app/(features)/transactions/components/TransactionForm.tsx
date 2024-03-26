71adec9b1c8d1a1e43f75323ea571c9aa21383b9
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-03-26 20:12:35 +0630

    move transaction form

---
+++ src/app/(features)/transactions/components/TransactionForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useState, useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "primereact/button";
+import { useForm, FormProvider } from "react-hook-form";
+import { Dialog } from "primereact/dialog";
+import { CreateTransactionForm as components } from "@/constants/Transaction";
+import DynamicFormElement from "@/components/Form/DynamicFormElement";
+import { createTransction } from "@/services/transactionServices";
+import { FormFieldType } from "@/types/form";
+
+const TransactionForm = () => {
+  const methods = useForm({
+    defaultValues: useMemo(() => {
+      let arrToObject: Record<string, any> = {};
+      components.forEach((obj) => {
+        arrToObject[obj.name] = obj.defaultValue;
+      });
+      return arrToObject;
+    }, []),
+    shouldUnregister: true,
+  });
+  const [confirmVisible, setConfirmVisible] = useState(false);
+  const [apiData, setApiData] = useState<{ success?: boolean } | undefined>(
+    undefined
+  );
+  const { control, handleSubmit, reset } = methods;
+  const router = useRouter();
+
+  const onSubmit: () => void = handleSubmit(async (data: any) => {
+    const res = await createTransction({
+      ...data,
+      spentAt: new Date(data.spentAt),
+    });
+    setApiData(res);
+    setConfirmVisible(true);
+  });
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form action={onSubmit}>
+          <div className="p-4 grid gap-5">
+            {components?.map((field, index) => {
+              return (
+                <DynamicFormElement
+                  control={control}
+                  componentType={field.formProps.type as FormFieldType}
+                  key={index}
+                  {...field}
+                  defaultValue={field?.defaultValue}
+                />
+              );
+            })}
+            <div className="flex flex-row-reverse p-4">
+              <Button
+                type="submit"
+                label="Save"
+                className="!py-2 fixed bottom-2"
+              />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
+      <Dialog
+        header={apiData?.success === false ? "Failed!" : "Success!"}
+        visible={confirmVisible}
+        onHide={() => setConfirmVisible(false)}>
+        {apiData?.success ? (
+          <Button
+            label="Go Home"
+            onClick={() => router.push("/")}
+            className="!text-black"
+          />
+        ) : (
+          <div className="flex flex-col gap-4">
+            <p> Something went wrong.</p>
+            <Button
+              icon="pi pi-refresh"
+              label="Try Again"
+              type="button"
+              onClick={() => setConfirmVisible(false)}
+            />
+          </div>
+        )}
+      </Dialog>
+    </div>
+  );
+};
+
+export default TransactionForm;
